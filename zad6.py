252ecd1d2f788d665e626a5cb94fe300b45f5485
Karol Sorek <karol.sorek@edu.uekat.pl>
2023-12-18 16:27:13 +0100

    zadania

--- zad6.py
+++ zad6.py
@@ -1,10 +1,10 @@
-
 def polacz_i_przetworz(lista1, lista2):
     lista_polaczona = lista1 + lista2
     lista_unikalna = list(set(lista_polaczona))
-    lista_wynikowa = [element**3 for element in lista_unikalna]
+    lista_wynikowa = [element ** 3 for element in lista_unikalna]
 
     return lista_wynikowa
+
 
 lista1 = [1, 2, 3, 4]
 lista2 = [3, 4, 5, 6]
